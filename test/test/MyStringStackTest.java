3951d951679f0820f12083701dcde112a3b9d0c2
Samuel Sim <samuel.sim2@ncs.com.sg>
2022-06-28 14:54:53 +0800

    fail the test case testPush

--- test/test/MyStringStackTest.java
+++ test/test/MyStringStackTest.java
@@ -33,10 +33,13 @@ public class MyStringStackTest extends TestCase {
 	public void testPush() {
 		String o1 = null;  
 		MyStringStack stack = new MyStringStack();
-		assertTrue (stack.isEmpty());
+		// assertTrue (stack.isEmpty());
+		// stack.push (s1);
+        // stack.push (s2);
+        // assertFalse (stack.isEmpty());
+		MyStringStack stack = new MyStringStack();
 		stack.push (s1);
-        stack.push (s2);
-        assertFalse (stack.isEmpty());
+		assertTrue (stack.isEmpty());
 	}
 
 	@Test
